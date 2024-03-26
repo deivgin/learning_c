1ee3e6c73de10485a8be3e37bd3518b830a5f45e
deivgin <deivgin.work@gmail.com>
2024-03-26 08:55:43 +0200

    Add numbers program

---
+++ addNumbers.c
@@ -0,0 +1,20 @@
+# include <stdio.h>
+
+int main ( void )
+{
+	int integer1;
+	int integer2;
+	int sum;
+
+	printf("Enter first number:");
+	scanf("%d", &integer1);
+
+	printf("Enter first number:");
+	scanf("%d", &integer1);
+
+	sum = integer1 + integer2;
+
+	printf("Sum is %d", sum);
+
+	return 0;
+}
