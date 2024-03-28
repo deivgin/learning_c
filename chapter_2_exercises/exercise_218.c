504a1dfdef4edde649fd8c97af5863185208b947
deivgin <deivgin.work@gmail.com>
2024-03-28 08:57:58 +0200

    218

---
+++ chapter_2_exercises/exercise_218.c
@@ -0,0 +1,24 @@
+# include <stdio.h>
+
+int main ( void )
+{
+	int num1, num2;
+
+	printf("Enter first number:\n");
+	scanf("%d", &num1);
+
+	printf("Enter second number:\n");
+	scanf("%d", &num2);
+
+	if(num1 > num2){
+		printf("%d is larger\n", num1);
+	} 
+
+	if(num1 < num2){
+		printf("%d is larger\n", num2);
+	}
+
+	if(num1 == num2){
+		printf("Numbers are equal\n");
+	}
+}
